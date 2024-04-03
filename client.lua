afce9b7b4931333dee3e1c665295e9ada61df383
Seven <58103427+squarerootof49@users.noreply.github.com>
2024-04-03 16:22:12 +0200

    tweak(loop): moved dispatch outside of loop

--- client.lua
+++ client.lua
@@ -36,6 +36,10 @@ CreateThread(function()
     SetCreateRandomCopsNotOnScenarios(Config.disableCops)
     SetCreateRandomCopsOnScenarios(Config.disableCops)
 
+    if Config.disableDispatch then
+        disableDispatch()
+    end
+
     if Config.disableWantedLevel then SetMaxWantedLevel(0) end
 
     SetAudioFlag('PoliceScannerDisabled', not Config.disablePoliceScan)
@@ -59,51 +63,42 @@ CreateThread(function()
 
     local removedEntities
 
+    if not Config.loopedRemoval then return end
+    
     while true do
         Wait(Config.loopTime)
 
-        if Config.loopedRemoval then 
-            removedEntities = 0
-            local vehicles = GetGamePool("CVehicle")
-            for i = 1, #vehicles do
-                local vehicle = vehicles[i]
-                local model = GetEntityModel(vehicle)
+        removedEntities = 0
+        local vehicles = GetGamePool("CVehicle")
+        for i = 1, #vehicles do
+            local vehicle = vehicles[i]
+            local model = GetEntityModel(vehicle)
 
-                for _, modelName in ipairs(Config.disabledVehicles) do
-                    if model == GetHashKey(modelName) then
-                        removeVehicle(vehicle)
-                        removedEntities += 1
-                        break
-                    end
+            for _, modelName in ipairs(Config.disabledVehicles) do
+                if model == GetHashKey(modelName) then
+                    removeVehicle(vehicle)
+                    removedEntities += 1
+                    break
                 end
             end
+        end
+        if Config.debug then print("[Population Manager] Successfully removed "..removedEntities.." vehicles.") end
 
-            if Config.debug then print("[Population Manager] Successfully removed "..removedEntities.." vehicles.") end
 
+        removedEntities = 0
+        local peds = GetGamePool("CPed")
+        for i = 1, #peds do
+            local ped = peds[i]
+            local model = GetEntityModel(ped)
 
-            removedEntities = 0
-            local peds = GetGamePool("CPed")
-            for i = 1, #peds do
-                local ped = peds[i]
-                local model = GetEntityModel(ped)
-
-                for _, modelName in ipairs(Config.disabledPeds) do
-                    if model == GetHashKey(modelName) then
-                        removePed(ped)
-                        removedEntities += 1
-                        break
-                    end
+            for _, modelName in ipairs(Config.disabledPeds) do
+                if model == GetHashKey(modelName) then
+                    removePed(ped)
+                    removedEntities += 1
+                    break
                 end
             end
-
-            if Config.debug then print("[Population Manager] Successfully removed "..removedEntities.." peds.") end
         end
-
-        if Config.disableDispatch then
-            disableDispatch()
-        end
-
-        SetScenarioTypeEnabled("DRIVE", true)
-        SetScenarioTypeEnabled("WALK", true)
+        if Config.debug then print("[Population Manager] Successfully removed "..removedEntities.." peds.") end
     end
 end)
